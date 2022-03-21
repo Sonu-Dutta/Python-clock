92fca3aaf0241c540d5c65cfa2994dd59e33808a
Sonu Dutta <itsmesonudutta@gmail.com>
2022-03-21 18:32:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import string
+from tkinter import *
+from tkinter import font
+from tkinter.ttk import *
+
+from time import strftime
+
+root= Tk()
+root.title("Clock")
+
+def time():
+    string=strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000, time)
+
+label=Label(root, font=("bold",50), background="black",foreground="lightblue")
+label.pack(anchor='center')
+time()
+mainloop()
