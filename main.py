a8b5c18da1415bcd08e6398d0b95b537ea89e02c
Sonu Dutta <90707563+Sonu-Dutta@users.noreply.github.com>
2022-03-26 16:01:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def time():
     string=strftime('%H:%M:%S %p')
     label.config(text=string)
     label.after(1000, time)
-
 label=Label(root, font=("bold",50), background="black",foreground="lightblue")
 label.pack(anchor='center')
 time()
